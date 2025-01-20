673827a027707218cb858a55fdd60d68cf4692c5
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-20 15:01:47 +0700

    logging

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,23 @@ from app.scheduler import scheduler
 from app.routes import register_routes
 
 # Настройка логирования
-logging.basicConfig(
-    level=logging.DEBUG,  # Уровень логирования
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Формат сообщений
-    handlers=[
-        logging.FileHandler("app.log", mode="a"),  # Запись в файл
-        logging.StreamHandler()  # Вывод в консоль
-    ]
-)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)  # Установите нужный уровень
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+
+# Добавьте обработчик к корневому логгеру
+logging.getLogger().addHandler(console_handler)
 
 
 def create_app():
     app = Flask(__name__)
+
+    # Интеграция логов Flask и Gunicorn
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
     scheduler.start()
     register_routes(app)
     return app
--- app/scheduler.py
+++ app/scheduler.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_EXECUTED
 from dotenv import load_dotenv
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -17,5 +18,20 @@ jobstores = {
 # Создание планировщика с хранилищем
 scheduler = BackgroundScheduler(jobstores=jobstores)
 
+
+# Настройка логирования APScheduler
+scheduler._logger = logging.getLogger("apscheduler")
+scheduler._logger.setLevel(logging.DEBUG)
+
+
+def job_listener(event):
+    if event.exception:
+        logging.error(f"""Job {event.job_id} failed with exception: {
+                      event.exception}""")
+    else:
+        logging.info(f"Job {event.job_id} executed successfully.")
+
+
+scheduler.add_listener(job_listener, EVENT_JOB_ERROR | EVENT_JOB_EXECUTED)
 
 logging.info("Scheduler initialized with PostgreSQL-backed JobStore")
